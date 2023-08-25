4e29f2592d868e6e89d7916967e869c6278fcfdc
acresion <dev@acresion.com>
2023-08-25 16:42:19 -0400

    finished testing the tenant

--- landlord-student-rental-dao/src/test/java/landlord/student/rental/daowork/TenantDAOImplementationTest.java
+++ landlord-student-rental-dao/src/test/java/landlord/student/rental/daowork/TenantDAOImplementationTest.java
@@ -109,22 +109,76 @@ class TenantDAOImplementationTest {
 		// verification
        ArgumentCaptor<Integer> indexCapture = ArgumentCaptor.forClass(Integer.class);
        ArgumentCaptor<Integer> valueIntCapture = ArgumentCaptor.forClass(Integer.class);
+       ArgumentCaptor<Integer> stringCapture = ArgumentCaptor.forClass(Integer.class);
+       ArgumentCaptor<String> valueStringCapture = ArgumentCaptor.forClass(String.class);
        //The reason why we call the times(7) is that we want it to invoke this verification for as many times as there are integers in the method(which there are 7)
 		verify(p, times(7)).setInt(indexCapture.capture(), valueIntCapture.capture());
 		verify(p).execute();
+		verify(p, times(6)).setString(stringCapture.capture(), valueStringCapture.capture());
 		assertEquals(1, indexCapture.getAllValues().get(0));
 		assertEquals(1, valueIntCapture.getAllValues().get(0));
-	}
+		assertEquals(2, stringCapture.getAllValues().get(0));
+		assertEquals("Alex", valueStringCapture.getAllValues().get(0));
 
+
+	}
+	
 	@Test
 	void testUpdateTenant() throws SQLException {
-		fail("Not yet implemented");
-
+		
+		//setup
+		Connection con = mock(Connection.class);
+		PreparedStatement p = mock(PreparedStatement.class);
+		when(con.prepareStatement(anyString())).thenReturn(p);
+		Tenant tenant = new Tenant.TenantBuilder(1).firstName("Alex").lastName("Albon").addressBeforeMovein(null)
+				.cityBeforeMovein("Paris").stateBeforeMovein(null).countryBeforeMovein("France").yearOfBirth(1995)
+				.monthOfBirth(12).dayOfBirth(3).room(503).floor(5).landlordId(1).build();
+		
+		//test
+		TenantDAOImplementation tenantDAO = new TenantDAOImplementation();
+		Tenant updatedTenant = new Tenant.TenantBuilder(1).firstName("Fernando").lastName("Alonso").addressBeforeMovein(null)
+				.cityBeforeMovein("Barcelona").stateBeforeMovein(null).countryBeforeMovein("Spain").yearOfBirth(1995)
+				.monthOfBirth(12).dayOfBirth(3).room(503).floor(5).landlordId(1).build();
+		tenantDAO.updateTenant(con, updatedTenant);
+		
+		//verification
+		 ArgumentCaptor<Integer> indexCapture = ArgumentCaptor.forClass(Integer.class);
+	       ArgumentCaptor<Integer> valueIntCapture = ArgumentCaptor.forClass(Integer.class);
+	       ArgumentCaptor<Integer> stringCapture = ArgumentCaptor.forClass(Integer.class);
+	       ArgumentCaptor<String> valueStringCapture = ArgumentCaptor.forClass(String.class);
+	       //The reason why we call the times(7) is that we want it to invoke this verification for as many times as there are integers in the method(which there are 7)
+			verify(p, times(7)).setInt(indexCapture.capture(), valueIntCapture.capture());
+			verify(p).execute();
+			verify(p, times(6)).setString(stringCapture.capture(), valueStringCapture.capture());
+			assertEquals(7, indexCapture.getAllValues().get(0));
+			assertEquals(1995, valueIntCapture.getAllValues().get(0));
+			assertEquals(1, stringCapture.getAllValues().get(0));
+			assertEquals("Fernando", valueStringCapture.getAllValues().get(0));
 	}
 
 	@Test
-	void testDeleteTenant() {
-		fail("Not yet implemented");
+	void testDeleteTenant() throws SQLException {
+		//setup
+		Connection con = mock(Connection.class);
+		PreparedStatement p = mock(PreparedStatement.class);
+		when(con.prepareStatement(anyString())).thenReturn(p);
+		Tenant tenant = new Tenant.TenantBuilder(1).firstName("Alex").lastName("Albon").addressBeforeMovein(null)
+				.cityBeforeMovein("Paris").stateBeforeMovein(null).countryBeforeMovein("France").yearOfBirth(1995)
+				.monthOfBirth(12).dayOfBirth(3).room(503).floor(5).landlordId(1).build();
+		
+		// test
+		TenantDAOImplementation tenantDAO = new TenantDAOImplementation();
+		tenantDAO.deleteTenant(con, 1);
+
+
+		
+		// verification
+		ArgumentCaptor<Integer> indexCapture = ArgumentCaptor.forClass(Integer.class);
+	       ArgumentCaptor<Integer> valueIntCapture = ArgumentCaptor.forClass(Integer.class);
+			verify(p, times(1)).setInt(indexCapture.capture(), valueIntCapture.capture());
+			verify(p).execute();
+			assertEquals(1, indexCapture.getAllValues().get(0));
+			assertEquals(1, valueIntCapture.getAllValues().get(0));	
 	}
 
 }
